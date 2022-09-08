f87b0e2d299abc3085f0b1c4aed38453337430c1
Ali Hajeh <aly.lhaj@gmail.com>
2022-09-08 23:12:37 +0300

    hah

---
+++ build/precache-manifest.e60dc196359aaafd5fc2300104959b3f.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4dd1a27a647dbaf194a2f155b86425b3",
+    "url": "/boutiquebeyrouth/index.html"
+  },
+  {
+    "revision": "210e695fd80488a2cd54",
+    "url": "/boutiquebeyrouth/static/css/main.de9dd1de.chunk.css"
+  },
+  {
+    "revision": "74edcf31c2cf037f7449",
+    "url": "/boutiquebeyrouth/static/js/2.66e51109.chunk.js"
+  },
+  {
+    "revision": "117d65892edc8adcd29d4a0d57b5a80a",
+    "url": "/boutiquebeyrouth/static/js/2.66e51109.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "210e695fd80488a2cd54",
+    "url": "/boutiquebeyrouth/static/js/main.73e1ea2f.chunk.js"
+  },
+  {
+    "revision": "1e71f5a8537ef960916f",
+    "url": "/boutiquebeyrouth/static/js/runtime-main.8623e0bb.js"
+  },
+  {
+    "revision": "c0fc859261875e95d6bf7034d6a5fca7",
+    "url": "/boutiquebeyrouth/static/media/1.c0fc8592.jpg"
+  },
+  {
+    "revision": "3ce357e71a836a86691b904396a8955c",
+    "url": "/boutiquebeyrouth/static/media/cta-illustration.3ce357e7.svg"
+  },
+  {
+    "revision": "0f9928d77bc67692e90527ec092fcbd0",
+    "url": "/boutiquebeyrouth/static/media/feature-tile-icon-01.0f9928d7.svg"
+  },
+  {
+    "revision": "bd39f30434175e7c8b1d1c74fa6569fd",
+    "url": "/boutiquebeyrouth/static/media/feature-tile-icon-02.bd39f304.svg"
+  },
+  {
+    "revision": "66f37ba5c7e4b6648f04883fa637e634",
+    "url": "/boutiquebeyrouth/static/media/feature-tile-icon-03.66f37ba5.svg"
+  },
+  {
+    "revision": "836acd100a942420f8ab4e6c5e4162d8",
+    "url": "/boutiquebeyrouth/static/media/feature-tile-icon-04.836acd10.svg"
+  },
+  {
+    "revision": "fa9ba00b178abc7bd2e02bd986e91fe3",
+    "url": "/boutiquebeyrouth/static/media/feature-tile-icon-05.fa9ba00b.svg"
+  },
+  {
+    "revision": "6a1776963deb39d710bae80bb449aebe",
+    "url": "/boutiquebeyrouth/static/media/feature-tile-icon-06.6a177696.svg"
+  },
+  {
+    "revision": "2554a674ffec1d72db6375f5fd1c11c6",
+    "url": "/boutiquebeyrouth/static/media/illustration-section-01.2554a674.svg"
+  },
+  {
+    "revision": "63d0555d90c053eea1a1a7469d004661",
+    "url": "/boutiquebeyrouth/static/media/illustration-section-02.63d0555d.svg"
+  },
+  {
+    "revision": "00127edc3e5cc6b11099cf080c1f635a",
+    "url": "/boutiquebeyrouth/static/media/logo.00127edc.png"
+  }
+]);
